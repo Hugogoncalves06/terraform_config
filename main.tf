10216c1c9c673ce9ae3b85ce7fbb2de592fd6798
HugoGoncalves06 <hugogoncalves06@ynov.com>
2025-06-30 13:51:31 +0200

    terraform init with api

---
+++ main.tf
@@ -0,0 +1,107 @@
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {}
+
+# MongoDB
+resource "docker_image" "mongo" {
+  name         = "mongo:6.0"
+  keep_locally = false
+}
+
+resource "docker_container" "mongo" {
+  image = docker_image.mongo.image_id
+  name  = "mongo"
+  ports {
+    internal = 27017
+    external = 27017
+  }
+  env = [
+    "MONGO_INITDB_DATABASE=blog-api"
+  ]
+  volumes {
+    host_path      = "${path.module}/mongo_data"
+    container_path = "/data/db"
+  }
+  restart = "unless-stopped"
+}
+
+# MySQL
+resource "docker_image" "mysql" {
+  name         = "mysql:8.0"
+  keep_locally = false
+}
+
+resource "docker_container" "mysql" {
+  image = docker_image.mysql.image_id
+  name  = "mysql"
+  ports {
+    internal = 3306
+    external = 3306
+  }
+  env = [
+    "MYSQL_DATABASE=users_db",
+    "MYSQL_USER=admin",
+    "MYSQL_PASSWORD=password",
+    "MYSQL_ROOT_PASSWORD=password"
+  ]
+  volumes {
+    host_path      = "${path.module}/mysql_data"
+    container_path = "/var/lib/mysql"
+  }
+  restart = "unless-stopped"
+}
+
+# API Node.js
+resource "docker_image" "api_nodejs" {
+  name         = "hugogoncalves06/ci_cd_nodejs_api_ynov:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "api_nodejs" {
+  image = docker_image.api_nodejs.image_id
+  name  = "api-nodejs"
+  ports {
+    internal = 8080
+    external = 8080
+  }
+  env = [
+    "NODE_ENV=development",
+    "PORT=8080",
+    "MONGODB_URI=mongodb://mongo:27017/blog-api",
+    "JWT_SECRET=your-super-secret-jwt-key-change-in-production",
+    "JWT_EXPIRES_IN=24h",
+    "RATE_LIMIT_WINDOW_MS=900000",
+    "RATE_LIMIT_MAX_REQUESTS=100"
+  ]
+  depends_on = [docker_container.mongo]
+}
+
+# API Python
+resource "docker_image" "api_python" {
+  name         = "hugogoncalves06/ci_cd_backend_flask_ynov:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "api_python" {
+  image = docker_image.api_python.image_id
+  name  = "api-python"
+  ports {
+    internal = 8000
+    external = 8000
+  }
+  env = [
+    "MYSQL_HOST=mysql",
+    "MYSQL_USER=admin",
+    "MYSQL_PASSWORD=password",
+    "MYSQL_DATABASE=users_db",
+    "FLASK_ENV=development"
+  ]
+  depends_on = [docker_container.mysql]
+} 
